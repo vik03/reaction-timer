f709d3b4c2d7fad673f9681fd5a062fa1eaf4606
vik03 <39328594+vik03@users.noreply.github.com>
2021-06-04 19:25:13 +0530

    Final Commit

---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>Reaction Time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.score < 250) {
+            this.rank = 'Awesome Pace'
+        } else if (this.score < 400) {
+            this.rank = "You will be Better at it, Try Again !!!"
+        } else {
+            this.rank = "Snail Pace"
+        }
+    }
+
+}
+</script>
+
+<style>
+  .rank{
+      font-size: 1.4em;
+      color: #0faf87;
+      font-weight: bold;
+  }
+
+</style>
